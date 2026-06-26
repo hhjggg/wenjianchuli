56ed67345d0b20b15ac0fc573d01f59b95b779de
Let_it_go <50875873+hhjggg@users.noreply.github.com>
2026-06-26 19:12:08 +0800

    Add files via upload
    文件上传

---
+++ wenj.py
@@ -0,0 +1,132 @@
+from openpyxl import load_workbook
+import streamlit as st
+import pandas as pd
+from io import BytesIO
+
+# ========== 网页界面 ==========
+st.title("订单SN数据回填工具")
+st.subheader("请上传两份Excel文件")
+
+# 文件上传组件
+sn_file = st.file_uploader("① 上传SN数据源表", type=["xlsx"])
+target_file = st.file_uploader("② 上传待回填目标表", type=["xlsx"])
+
+# 两个文件都上传后才执行处理
+if sn_file is not None and target_file is not None:
+    # 读取上传的表格
+    df_sn = pd.read_excel(sn_file)
+    df_target = pd.read_excel(target_file)
+    st.success("文件读取完成，开始处理数据！")
+
+        # ===================== 文件路径 =====================
+    PATH_TARGET = "订单号和写入.xlsx"    # 待回填目标表
+    PATH_SN_SOURCE = "sn数据来源.xlsx"# SN数据源表
+    # ==================================================
+
+    # 1、构建带订单号+sku+sn的完整字典
+    wb_sn_source = load_workbook(PATH_SN_SOURCE)
+    ws_sn_source = wb_sn_source.active
+    sn_mapping = {}
+    over_three_sn_order = []
+
+    for row in range(2, ws_sn_source.max_row + 1):
+        jd_order = ws_sn_source.cell(row=row, column=5).value
+        sku_id = ws_sn_source.cell(row=row, column=3).value
+        sn_code = ws_sn_source.cell(row=row, column=10).value
+
+        match_key = str(jd_order).strip()
+        if sn_code is not None and str(sn_code).strip() != "":
+            if match_key not in sn_mapping:
+                sn_mapping[match_key] = []
+            sn_mapping[match_key].append((sku_id, sn_code))
+
+    # 遍历字典，筛选有效SN数量>=3的订单
+    for order, sku_sn_list in sn_mapping.items():
+        if len(sku_sn_list) >= 3:
+            over_three_sn_order.append(order)
+
+    wb_sn_source.close()
+
+        #print("==================== 完整订单-SKU-SN字典数据 ====================")
+        # 循环遍历字典每一条订单
+        #for order_num, sku_sn_list in sn_mapping.items():
+        #    print(f"订单号：{order_num} ｜ 对应SKU+SN列表：{sku_sn_list}")
+        #print("================================================================\n")
+
+    # ---------------------- 输出2：打印有效SN数量≥3的订单号 ----------------------
+    print("==================== SN数量≥3的订单号清单 ====================")
+    if len(over_three_sn_order) == 0:
+        print("不存在有效SN数量大于等于3条的订单")
+    else:
+        for idx, order in enumerate(over_three_sn_order, start=1):
+            print(f"{idx}. 订单号：{order}，有效SN条数：{len(sn_mapping[order])}")
+    print(f"合计SN≥3条的订单总数：{len(over_three_sn_order)}")
+    print("================================================================\n")
+
+
+    success_pair_list = []
+    # 2、遍历目标表回填G/H列
+    wb_target = load_workbook(PATH_TARGET)
+    ws_target = wb_target.active
+
+    for row in range(2, ws_target.max_row + 1):
+        tri_order = ws_target.cell(row=row, column=3).value
+        ws_target.cell(row=row, column=7, value=None)
+        ws_target.cell(row=row, column=8, value=None)
+        if tri_order is None:
+            continue
+        current_key = str(tri_order).strip()
+        if current_key not in sn_mapping:
+            continue
+        
+        data_list = sn_mapping[current_key]
+        success_pair_list.extend(data_list)
+        sn_count = len(data_list)
+        if sn_count == 1:
+            _, sn = data_list[0]
+            ws_target.cell(row=row, column=7, value=sn)
+        elif sn_count == 2:
+            data_sorted = sorted(data_list, key=lambda x: int(x[0]) if str(x[0]).isdigit() else 0)
+            _, sn_small = data_sorted[0]
+            _, sn_big = data_sorted[1]
+            ws_target.cell(row=row, column=7, value=sn_big)
+            ws_target.cell(row=row, column=8, value=sn_small)
+        #elif sn_count == 3:
+        #    data_two = data_list[:2]
+        #    data_sorted = sorted(data_two, key=lambda x: int(x[0]) if str(x[0]).isdigit() else 0)
+        #    _, sn_small = data_sorted[0]
+        #   _, sn_big = data_sorted[1]
+        #    ws_target.cell(row=row, column=7, value=sn_big)
+        #   ws_target.cell(row=row, column=8, value=sn_small)
+        elif sn_count >= 3:
+            # SN数量≥3：SN条数数字填入G内机，H外机空
+            ws_target.cell(row=row, column=7, value=sn_count)
+            continue
+
+    wb_target.save(PATH_TARGET)
+    wb_target.close()
+    print("处理完成！")
+
+
+    # ---------------------- 输出2：打印所有配对成功的(sku,SN)元组 ----------------------
+    print("==================== 所有匹配成功的(sku, SN)元组明细 ====================")
+    if len(success_pair_list) == 0:
+        print("暂无匹配成功的SKU-SN配对数据")
+    else:
+        for idx, pair in enumerate(success_pair_list, start=1):
+            print(f"第{idx}组配对：{pair}")
+
+        output = BytesIO()
+        with pd.ExcelWriter(output, engine="openpyxl") as writer:
+            df_target.to_excel(writer, index=False)
+        output.seek(0)
+
+    # 网页下载按钮
+    st.download_button(
+        label="📥 下载处理完成Excel",
+        data=output,
+        file_name="回填完成_订单表.xlsx",
+        mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    )
+else:
+    st.info("请依次上传两份xlsx格式Excel文件")
